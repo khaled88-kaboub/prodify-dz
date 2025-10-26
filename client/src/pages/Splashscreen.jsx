3ad08849d1ed043f5727ee0da47b15e1d085210a
khaled88-kaboub <khaledkaboub88@gmail.com>
2025-10-26 20:04:28 +0100

    Update Splashscreen.jsx

--- client/src/pages/Splashscreen.jsx
+++ client/src/pages/Splashscreen.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./SplashScreen.css";
+import "./Splashscreen.css";
 
 const SplashScreen = () => {
   const [progress, setProgress] = useState(0);
