0133d0bd073b6f581abd04ae1e07c6a4f41c7d03
kaboub khaled <khaledkaboub88@gmail.com>
2026-05-11 18:33:56 +0100

    modifier le aios

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // === CONFIG DE BASE ===
 // 🔧 Change l’URL selon ton backend :
-const API_URL = "https://prodify-dz.onrender.com"; // ou ton URL Render/Netlify
+const API_URL = "https://prodify-dz.onrender.com/api"; // ou ton URL Render/Netlify
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://prodify-dz.onrender.com",
+  baseURL: "https://prodify-dz.onrender.com/api/",
 });
 
 api.interceptors.request.use((config) => {
